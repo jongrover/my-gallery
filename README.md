ffdc91e54612c71cca1d95542e9a37f4140d2f1b
jongrover <hello@jonathangrover.com>
2015-10-22 19:40:49 -0400

    finish gallery

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Gallery
+
+Custom jQuery gallery with thumbnails that brighten on hover and modal when clicked.
+Makes use of fancybox plugin.
---
+++ js/gallery.js
@@ -0,0 +1,24 @@
+$(function(){
+
+  var $thumbnails = $('#gallery img');
+
+  //Thumbnail fading
+
+  $thumbnails.fadeTo(200, .5);
+
+  $thumbnails.hover(function(){
+    // mouseover
+    $(this).fadeTo(600, 1);
+  }, function(){
+    //mouseoff
+    $(this).fadeTo(400, .5);
+  });
+
+
+  // Image Modal
+
+  
+
+
+
+});
